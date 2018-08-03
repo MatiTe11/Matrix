81a771b34a1cbd917b8a052a8f5fa707a9d34422
Mateusz Tęgowski <m.tegowski@wp.pl>
2018-08-03 13:14:49 +0200

    Add project files.

---
+++ MatrixMultiplication/Matrix.h
@@ -0,0 +1,12 @@
+#pragma once
+class Matrix
+{
+private:
+	int size;
+	double** A;
+
+public:
+	Matrix();
+	~Matrix();
+};
+
---
+++ MatrixMultiplication/main.cpp
@@ -0,0 +1,1 @@
+///vector of strings containing equations is converted int Matrix objects, which then can be multiplied using operator* (Jacobi parallel)
